ab419338bd593e363565fbf30b615f9f7590a657
tariktrokic <tariktrokic@gmail.com>
2024-07-03 09:21:59 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,104 @@
+"use client";
+
 import Image from "next/image";
+import { CarbonConnect, EmbeddingGenerators } from "carbon-connect";
+import axios from "axios";
+import { Carbon } from "carbon-typescript-sdk";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [carbon, setCarbon] = useState(null);
+
+  const carbonSetupAccessToken = async () => {
+    const carbonWithApiKey = new Carbon({
+      apiKey: "<API_KEY_HERE",
+      customerId: "111111",
+    });
+
+    const accessToken = await carbonWithApiKey.auth.getAccessToken();
+
+    const carbon = new Carbon({
+      accessToken: accessToken.data.access_token,
+    });
+
+    setCarbon(carbon);
+
+    return {
+      access_token: accessToken.data.access_token,
+    };
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <CarbonConnect
+        orgName="Test"
+        brandIcon="path/to/your/brand/icon"
+        embeddingModel={EmbeddingGenerators.OPENAI_ADA_LARGE_1024}
+        tokenFetcher={carbonSetupAccessToken}
+        tags={{
+          kb1: "tag1_value",
+          tag2: "tag2_value",
+          tag3: "tag3_value",
+        }}
+        useRequestIds={true}
+        // showFilesTab={false}
+        maxFileSize={10000000}
+        enabledIntegrations={[
+          {
+            id: "LOCAL_FILES",
+            chunkSize: 100,
+            overlapSize: 10,
+            maxFileSize: 20000000,
+            allowMultipleFiles: true,
+            maxFilesCount: 5,
+            allowedFileTypes: [
+              {
+                extension: "csv",
+                chunkSize: 1200,
+                overlapSize: 120,
+                embeddingModel: "OPENAI",
+              },
+              {
+                extension: "txt",
+                chunkSize: 1599,
+                overlapSize: 210,
+                embeddingModel: "AZURE_OPENAI",
+              },
+              {
+                extension: "pdf",
+              },
+            ],
+          },
+          {
+            id: "NOTION",
+            chunkSize: 1500,
+            overlapSize: 20,
+            embeddingModel: "OPENAI",
+          },
+          {
+            id: "WEB_SCRAPER",
+            chunkSize: 1500,
+            overlapSize: 20,
+          },
+          {
+            id: "GOOGLE_DRIVE",
+            chunkSize: 1000,
+            overlapSize: 20,
+          },
+        ]}
+        onSuccess={(data) => console.log("Data on Success: ", data)}
+        onError={(error) => console.log("Data on Error: ", error)}
+        primaryBackgroundColor="#F2F2F2"
+        primaryTextColor="#000000"
+        secondaryBackgroundColor="#f2f2f2"
+        secondaryTextColor="#000000"
+        allowMultipleFiles={true}
+        open={true}
+        chunkSize={1500}
+        overlapSize={20}
+        // entryPoint="LOCAL_FILES"
+      ></CarbonConnect>
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -27,7 +123,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -38,7 +133,6 @@ export default function Home() {
           priority
         />
       </div>
-
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
